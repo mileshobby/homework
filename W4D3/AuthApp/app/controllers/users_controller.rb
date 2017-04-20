fa0a05d7b8ecfc466b8e417adfa3b6f3fb195e23
Miles Hobby <mhobby16@gmail.com>
2017-04-19 19:25:34 -0700

    add W4D3

--- W4D3/AuthApp/app/controllers/users_controller.rb
+++ W4D3/AuthApp/app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :session_token)
   end
 
 end
