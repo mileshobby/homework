14f4e7aed016582322b112a9a3afce100cd96e87
Miles Hobby <mhobby16@gmail.com>
2017-05-04 21:58:50 -0700

    configure NPM

---
+++ W6D5/skeleton/webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+module.exports = {
+  entry: "./entry.jsx",
+  output: {
+    // path: path.resolve(__dirname, 'app,', '')
+    filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  module: {
+  loaders: [
+    {
+      test: [/\.jsx?$/], // Specifies file types to transpile
+      exclude: /(node_modules)/, // Leaves dependencies alone
+      loader: 'babel-loader', // Sets Babel as the transpiler
+      query: {
+        presets: ['es2015', 'react'] // Tells Babel what syntaxes to translate
+      }
+    }
+  ]
+},
+resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
