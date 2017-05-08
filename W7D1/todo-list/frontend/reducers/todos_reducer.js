de4759ed17626bd6c11c5622bc1a1a1738c3c555
Miles Hobby <mhobby16@gmail.com>
2017-05-07 17:21:04 -0700

    add todosReducer

---
+++ W7D1/todo-list/frontend/reducers/todos_reducer.js
@@ -0,0 +1,10 @@
+import {RECIEVE_TODOS, RECIEVE_TODO, recieveTodos, recieveTodo} from 'todo_actions';
+
+const todosReducer = (state = {}, action) =>{
+  switch(action.type){
+    case RECIEVE_TODOS:
+      
+    default:
+      return state;
+  }
+};
